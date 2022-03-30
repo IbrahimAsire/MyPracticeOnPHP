202ffce23fd8b2a0118ca508db9a8400e2429a90
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-03-30 15:19:17 +0300

    feat: try array

--- php1.php
+++ php1.php
@@ -6,8 +6,14 @@
 $greet = "Hello PHP!";
 echo $greet;
 echo '<br>';
-define("Greeting", "<br>Welcome<br> to<br> PHP!");
+define("Greeting", "<br>Welcome<br> to<br> PHP!<br><br>");
 echo Greeting;
+echo "<br> --------------------------<br>";
+$arrayList = array("Ibrhim", "Ahmed", "Asiri");
+foreach($arrayList as $arr){
+    echo $arr."<br>";
+}
+
 ?> 
 
 </body>
