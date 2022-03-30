ee86d0983b3be31da310771598914ce245d37589
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-03-30 14:05:38 +0300

    first try on PHP

---
+++ php1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body><title> HI </title>
+
+<?php
+$greet = "Hello PHP!";
+echo $greet;
+echo '<br>';
+define("Greeting", "Welcome to PHP!");
+echo Greeting;
+?> 
+
+</body>
+</html>
