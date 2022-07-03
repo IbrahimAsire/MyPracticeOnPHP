aa023c4282689ae85bba41a7a6197f0ed2070653
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-07-04 01:18:38 +0300

    still practice in cond page

--- conditional.php
+++ conditional.php
@@ -11,5 +11,22 @@ if($x > $y) {
     echo "they are equal ..";
 }
 
+echo "<br>-----------------------<br>";
+
+$session1 = 1;
+$session2 = 1;
+$session3 = 1;
+$session4 = 1;
+$session5 = 0;
+$session6 = 0;
+
+$totalSession = $session1 + $session2 + $session3 + $session4 + $session5 + $session6;
+
+if($totalSession >= 4) {
+    echo "you are persented ". $totalSession . ", you are in save area";
+}
+
+
+
 
 ?>
