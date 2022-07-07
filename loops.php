f036571916a2fa0f2862ee70f09775666fe2a18c
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-07-07 18:15:31 +0300

    contunie with loops page

--- loops.php
+++ loops.php
@@ -6,7 +6,21 @@ for($i=0; $i <= 10; $i++){
     echo $i. "<br>";
 }
 
+echo "<h2>ASD</h2>";
+$j = 10;
+while($j >= 1){
+    echo $j. "<br>";
+    $j--;
+}
 
+echo "<h3>1.2.3: </h3>";
+
+for($x=1; $x <= 3; $x++){
+    echo $x. "<br>";
+    for($y=1; $y <=3; $y++){
+        echo " ".  $x. ".". $y. "<br>";
+    }
+}
 
 
 
