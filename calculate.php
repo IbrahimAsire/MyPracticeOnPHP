c1e5a61da9503a3c9cdd33d325f9726d28db3427
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-07-12 16:06:25 +0300

    fix: complete the opreation in calcuate page

--- calculate.php
+++ calculate.php
@@ -1,5 +1,20 @@
 <?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$operation = $_POST['operate'];
 
+echo $operation. "<br>";
 
+if($operation == 'sum'){
+    $cal_values = $num1 + $num2;
+} else if($operation == 'sub'){
+    $cal_values = $num1 - $num2;
+} else if($operation == 'multip'){
+    $cal_values = $num1 * $num2;
+} else{
+    $cal_values = $num1 / $num2;
+}
+
+echo $cal_values;
 
 ?>
