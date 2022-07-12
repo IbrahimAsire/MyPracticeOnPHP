faad14a55268110b470d575227a9ab74869da7e1
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-07-12 21:27:57 +0300

    feat: add page to some of math function

--- calculate.php
+++ calculate.php
@@ -7,7 +7,7 @@ if(isset($_POST['num1']) && isset($_POST['num2']) && !empty($_POST['num1']) && !
     $operation = $_POST['operate'];
 
     echo $operation. "<br>";
-     
+
     // To make sure that the input is number(int)
     if(is_int($_POST['num1']) && is_int($_POST['num2'])){
         /*if($operation == 'sum'){
@@ -32,8 +32,8 @@ if(isset($_POST['num1']) && isset($_POST['num2']) && !empty($_POST['num1']) && !
                 $cal_values = $num1 * $num2;
                 break;
             default:
-            $cal_values = $num1 / $num2;
-            break;
+                $cal_values = $num1 / $num2;
+                break;
         }
 
         echo $cal_values;
---
+++ mathFunc.php
@@ -0,0 +1,14 @@
+<?php
+
+$x =-1;
+echo abs($x);
+echo "<br>";
+echo rand(0, 10);
+echo "<br>";
+echo max(1, 3, 4, 14, 13);
+echo "<br>";
+echo min(-1, 23, 32, 37);
+echo "<br>";
+echo sqrt(9);
+
+?>
