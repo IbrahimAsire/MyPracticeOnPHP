61a414be41c6a9030399d4dd049c5f9a309033ce
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-07-14 19:25:26 +0300

    feat: create array func page

---
+++ arraysFunc.php
@@ -0,0 +1,17 @@
+<?php
+$nameArr = array('Ibrahim', 'Ahmed', 'Fahd', 'Khaled', 'Moh', 'Abdu');
+$numArr = array(23, 25, 22, 1, 6, 8, 7, 23, 17, 7, 6, 23, 7);
+
+$agesArr = array(
+    'Ibra' => 23,
+    'Fahd' => 24,
+    'Ahmed' => 37,
+    'Moh' => 33
+);
+
+
+
+
+
+
+?>
