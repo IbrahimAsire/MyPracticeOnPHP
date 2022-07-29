5c9ad015cb5b4c71830a4a071ed779e5d7ed7106
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-07-30 00:18:35 +0300

    create pages to try funciton

---
+++ toFunc.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>try some of function</title>
+    </head>
+    <body>
+        <div>
+            <form action="resultFunc.php" action="post"></form>
+            <label for="func1">Type a Text!</label>
+            <input type="text" name="func1" id="func1">
+            <br><br>
+            <button type="submit" name="submit" id="submit">SHOW the result</button>
+        </div>
+    </body>
+</html>
