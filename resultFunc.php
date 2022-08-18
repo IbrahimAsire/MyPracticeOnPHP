5850e152a1a1aab23aac09fcfb3323531fdbd913
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-08-18 16:42:15 +0300

    add post func in resultFunc page;

--- resultFunc.php
+++ resultFunc.php
@@ -3,12 +3,15 @@
 $result = $_POST['func1'];
 $test = $_POST['func2'];
 $num = $_POST['num'];
+$intersting = $_POST['func3'];
 
 echo strrev($result);
 echo "<br>____________<br>";
 echo $test;
 echo "<br> ------------<br>";
 echo var_dump($result);
+echo "<br>_____________<br>";
+echo $intersting;
 
 
 ?>
