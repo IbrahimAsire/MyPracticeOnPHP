884c37899e7552201afef572f4299878ecd4bd5b
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-08-31 20:28:57 +0300

    try get data from db

---
+++ category.php
@@ -0,0 +1,13 @@
+<?php
+include('config.php');
+
+$sql_statment = mysqli_query($connection, "SELECT * FROM category");
+$cat_nums = mysqli_num_rows($sql_statment);
+
+while($row = mysqli_fecth_array($sql_statment)) {
+    echo $row['id'] ." - ". $row['catname'] ."<br>";
+
+}
+
+
+?>
--- config.php
+++ config.php
@@ -1,15 +1,17 @@
 <?php
-$db_host = '';
-$db_user = '';
-$db_pass = '';
-$db_name = '';
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "learnphp";
 
 //stop error
-error_reporting(E_All ^ E_NOTICE);
+error_reporting(E_ALL ^ E_NOTICE);
 // error_reporting(E_All ^ E_DEPRECATED);
 
 //connent MySQL DataBase
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-mysqli_query($connection, "SET cgaracter_set_result = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8, character_set_server = 'utf8'");
+mysqli_query($connection, "SET character_set_results = 'utf8', character_set_client = 'utf8',
+                        character_set_connection = 'utf8', 
+                        character_set_database = 'utf8', character_set_server = 'utf8'");
 
 ?>
