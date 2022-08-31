4940bdb2d985c0879f2fc77dd3e569792e8802f4
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-08-31 20:50:54 +0300

    fix: resolve fetch data

--- category.php
+++ category.php
@@ -4,7 +4,7 @@ include('config.php');
 $sql_statment = mysqli_query($connection, "SELECT * FROM category");
 $cat_nums = mysqli_num_rows($sql_statment);
 
-while($row = mysqli_fecth_array($sql_statment)) {
+while($row = mysqli_fetch_array($sql_statment)) {
     echo $row['id'] ." - ". $row['catname'] ."<br>";
 
 }
