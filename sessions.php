22b8c27b01a3dfbbdc23108a9e853f5044db1e19
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-09-11 20:36:22 +0300

    add onter session fri ID in seesion page

--- sessions.php
+++ sessions.php
@@ -2,8 +2,9 @@
 session_start();
 
 $_SESSION['userName'] = "Ibrahim";
+$_SESSION['userID'] = "1212";
 
-echo "Welcome, ". $_SESSION['userName'];
+echo "Welcome, ". $_SESSION['userName']. "<br>". "Your ID is: ". $_SESSION['userID'];
 
 
 
