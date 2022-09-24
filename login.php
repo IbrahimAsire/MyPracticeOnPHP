b28fddf1e46277e0ad6997e509645cd5df052770
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-09-24 11:41:28 +0300

    feat: add feature

--- login.php
+++ login.php
@@ -1,6 +1,19 @@
 <?php
 
 
-
-
 ?>
+
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login Page</h1>
+        <div>
+            <form action="" method="POST" id="loginform" name="loginform">
+            <label for="username">UserName</label>
+            <input type="text" id="username" name="username">
+        </div>
+    </body>
+
+</html>
