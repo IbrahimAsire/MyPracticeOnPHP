f0e91a9fa415ac6a5daa106e4679083705db21a3
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-09-24 12:25:53 +0300

    feat: add feture to ensure the name and password not empty

--- login.php
+++ login.php
@@ -10,6 +10,12 @@
     <body>
         <h1>Login Page</h1>
         <div>
+            <?php
+            if(isset($_POST["username"]) && !empty($_POST["username"]) && isset($_POST["password"]) && !empty(["password"])) {
+                echo"Code will be here";
+            } else {
+                ?>
+            
             <form action="" method="POST" id="loginform" name="loginform">
             <label for="username">UserName</label>
             <input type="text" id="username" name="username">
@@ -18,6 +24,8 @@
             <input type="password" name="password" id="password">
             <br><br>
             <button type="submit" name="submit" value="submit">Login</button>
+            </form>
+            <?php } ?>
         </div>
     </body>
 
